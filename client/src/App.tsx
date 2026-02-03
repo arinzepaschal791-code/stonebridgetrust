f0f764193dfca56fe7fc4ebe6953fbf2667372d4
arinzepaschal791-code <arinzepaschal791@gmail.com>
2026-02-03 15:36:28 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-// Components
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-// Pages
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Transfer from './pages/Transfer';
+// Remove these if files don't exist:
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-gray-50">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard/*" element={<Dashboard />} />
-          </Routes>
-        </main>
-        <Footer />
-        <ToastContainer position="top-right" autoClose={3000} />
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="transfer" element={<Transfer />} />
+          {/* Remove these if files don't exist: */}
+          {/* <Route path="about" element={<About />} /> */}
+          {/* <Route path="services" element={<Services />} /> */}
+          {/* <Route path="contact" element={<Contact />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
