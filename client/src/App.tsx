d30d62a05f2810c46cbb045c0a8fe052a03b33e8
arinzepaschal791-code <arinzepaschal791@gmail.com>
2026-02-04 09:39:54 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import Transfer from './pages/Transfer';
-// Remove these if files don't exist:
-// import About from './pages/About';
-// import Services from './pages/Services';
-// import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="login" element={<Login />} />
-          <Route path="transfer" element={<Transfer />} />
-          {/* Remove these if files don't exist: */}
-          {/* <Route path="about" element={<About />} /> */}
-          {/* <Route path="services" element={<Services />} /> */}
-          {/* <Route path="contact" element={<Contact />} /> */}
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        {/* Add other routes here */}
       </Routes>
     </BrowserRouter>
   );
